532572d48a23fab643387c46b8c6d5b3218a09ac
Florian Fuchs <florian.fuchs@viovendi.de>
2020-01-08 12:37:00 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# sqs-message-requeue
+# SQS Message Requeue
+
+Python Lambda function for handling SQS Messages.
+
+Heavily inspired by https://github.com/tomelliff/sqs-dead-letter-requeue
+
+## Requirements
+
+* Python 2.7
+
+## Running it locally
+
+### Local setup/configuration
+
+Make sure you have AWS credentials configured and a default region specified.
+
+This can be with environment variables:
+
+```sh
+export AWS_ACCESS_KEY_ID=<my-access-key>
+export AWS_SECRET_ACCESS_KEY=<my-secret-key>
+export AWS_DEFAULT_REGION=eu-central-1
+```
+
+or setting them in either an AWS credentials file (~/.aws/credentials) or AWS config file (~/.aws/config):
+
+You will also need to set the name of the main queue to be requeued:
+
+```sh
+export DEST_Q_NAME=<my-queue-name>
+export SOURCE_Q_NAME=<my-queue-name>
+```
+
+### Virtualenv/dependencies
+```sh
+virtualenv env
+. env/bin/activate
+pip install boto3
+```
+
+### Run it
+check current definition of `initialize` in `requeue.py`
+```sh
+python requeue.py
+```
---
+++ requeue.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import boto3
+import os
+import json
+
+# default
+source_queue_name = None
+destination_queue_name = None
+moveMessage = None
+deleteMessage = None
+
+def requeueAllFromEnviron():
+    source_queue_name = os.environ['SOURCE_Q_NAME'] if 'SOURCE_Q_NAME' in os.environ else None
+    destination_queue_name = os.environ['DEST_Q_NAME'] if 'DEST_Q_NAME' in os.environ else None
+    moveMessage = everyMessage
+    deleteMessage = moveMessage
+    return source_queue_name, destination_queue_name, moveMessage, deleteMessage
+
+def requeueAllFromDeadLetter():
+    source_queue_name = 'worker-production2-dead-letter-queue'
+    destination_queue_name = 'worker-production2-queue'
+    moveMessage = everyMessage
+    deleteMessage = moveMessage
+    return source_queue_name, destination_queue_name, moveMessage, deleteMessage
+
+def requeueAllFromDeadLetterStaging1():
+    source_queue_name = 'worker-staging1-dead-letter-queue'
+    destination_queue_name = 'worker-staging1-queue'
+    moveMessage = everyMessage
+    deleteMessage = moveMessage
+    return source_queue_name, destination_queue_name, moveMessage, deleteMessage
+
+def requeueAllFromDeadLetterStaging1WithoutDeleting():
+    source_queue_name = 'worker-staging1-dead-letter-queue'
+    destination_queue_name = 'worker-staging1-queue'
+    moveMessage = everyMessage
+    deleteMessage = noMessage
+    return source_queue_name, destination_queue_name, moveMessage, deleteMessage
+
+def requeueOnlyWebhookPushFromDeadLetter():
+    source_queue_name = 'worker-production2-dead-letter-queue'
+    destination_queue_name = 'worker-production2-queue'
+    moveMessage = onlyWebhookMessages
+    deleteMessage = moveMessage
+    return source_queue_name, destination_queue_name, moveMessage, deleteMessage
+
+def deleteRegenerateFromDeadLetter():
+    source_queue_name = 'worker-production2-dead-letter-queue'
+    destination_queue_name = 'worker-production2-queue'
+    moveMessage = noMessage
+    deleteMessage = onlyRegenerateMessages
+    return source_queue_name, destination_queue_name, moveMessage, deleteMessage
+
+def moveRegenerateFromWorkerQueueToDeadLetter():
+    source_queue_name = 'worker-production2-queue'
+    destination_queue_name = 'worker-production2-dead-letter-queue'
+    moveMessage = onlyRegenerateMessages
+    deleteMessage = moveMessage
+    return source_queue_name, destination_queue_name, moveMessage, deleteMessage
+
+
+#initialize = moveRegenerateFromWorkerQueueToDeadLetter
+initialize = requeueAllFromDeadLetter
+
+
+
+def noMessage(message):
+    return False
+
+def everyMessage(message):
+    return True
+
+def onlyWebhookMessages(message):
+    message_json = json.loads(message.body)
+    if 'task' in message_json and message_json['task'] == 'webhook/push':
+        return True
+    return False
+
+def onlyRegenerateMessages(message):
+    message_json = json.loads(message.body)
+    if 'in' in message_json and 'regenerate' in message_json['in'] and message_json['in']['regenerate'] == True:
+        return True
+    return False
+
+
+def handler(event, context):
+    messages_moved = requeue_all_messages()
+
+    response = {
+        "statusCode": 200,
+        "body": "Total messages moved: {}".format(messages_moved)
+    }
+
+    return response
+
+def _get_sqs_queues():
+
+    sqs = boto3.resource('sqs')
+
+    destination_queue = sqs.get_queue_by_name(QueueName=destination_queue_name)
+    source_queue = sqs.get_queue_by_name(QueueName=source_queue_name)
+
+    return (destination_queue, source_queue)
+
+def requeue_all_messages(max_messages_per_poll=10, poll_wait=20, visibility_timeout=20):
+    destination_queue, source_queue = _get_sqs_queues()
+
+    total_messages_moved = 0
+
+    try:
+        while True:
+            messages = source_queue.receive_messages(
+                                        MaxNumberOfMessages=max_messages_per_poll,
+                                        WaitTimeSeconds=poll_wait,
+                                        VisibilityTimeout=visibility_timeout)
+            number_of_messages = len(messages)
+            if number_of_messages == 0:
+                print('No (more) messages in {} to check.'.format(source_queue_name))
+                break
+            else:
+                print('Checking {:2d} from {}, '.format(number_of_messages, source_queue_name), end = "")
+                requeued_messages_to_send = []
+                requeued_messages_to_delete = []
+                for message in messages:
+                    if moveMessage(message):
+                        requeued_messages_to_send.append({
+                            'Id': message.message_id,
+                            'MessageBody': message.body
+                        })
+                    if deleteMessage(message):
+                        requeued_messages_to_delete.append({
+                            'Id': message.message_id,
+                            'ReceiptHandle': message.receipt_handle
+                        })
+                print('requeuing {:2d} to {}, '.format(len(requeued_messages_to_send), destination_queue_name), end = "")
+                if requeued_messages_to_send:
+                    #for i in range(1, 500): 
+                        destination_queue.send_messages(Entries=requeued_messages_to_send)
+                print('deleting {:2d} from {}.'.format(len(requeued_messages_to_delete), source_queue_name))
+                if requeued_messages_to_delete:
+                    source_queue.delete_messages(Entries=requeued_messages_to_delete)
+
+            total_messages_moved += number_of_messages 
+    except KeyboardInterrupt:
+        pass
+
+    return total_messages_moved
+
+if __name__ == '__main__':
+    source_queue_name, destination_queue_name, moveMessage, deleteMessage = initialize()
+    requeue_all_messages()
